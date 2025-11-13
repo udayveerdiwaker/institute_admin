26d58b5704c5dbbffb696d9164b194f77b2115b8
ShivaHub <udayveerdiwaker2@gmail.com>
2025-11-14 00:16:44 +0530

    som

--- institute_db.sql
+++ institute_db.sql
@@ -1,21 +1,18 @@
--- CREATE DATABASE institute_db;
--- USE institute_db;
+CREATE DATABASE institute_db;
+USE institute_db;
 
--- CREATE TABLE students (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(100),
---     course VARCHAR(100),
---     fee DECIMAL(10,2),
---     date DATE
--- );
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+ 
+    date DATE
+);
 
 
--- CREATE TABLE courses (
---   id INT AUTO_INCREMENT PRIMARY KEY,
---   name VARCHAR(100) NOT NULL,
---   duration VARCHAR(50) NOT NULL,
---   fee DECIMAL(10,2) NOT NULL
--- );
-ALTER TABLE courses 
-ADD COLUMN duration VARCHAR(100) AFTER course,
-ADD COLUMN fees DECIMAL(10,2) AFTER duration;
+CREATE TABLE courses (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  course VARCHAR(100) NOT NULL,
+  duration VARCHAR(50) NOT NULL,
+  fee DECIMAL(10,2) NOT NULL,
+  date DATE
+);
